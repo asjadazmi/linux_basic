3d4f4becf5631fadbcca0175148a0139783778fe
asjadazmi <Asjadazmi0786@gmail.com>
2022-05-10 11:35:11 +0530

    updated

--- day_9/uc1_checkEmployee.sh
+++ day_9/uc1_checkEmployee.sh
@@ -9,3 +9,16 @@ else
     echo "employee is abscent";
 
 fi
+
+
+#!/bin/bash
+
+ispresent=1;
+random=$((RANDOM%2))
+if [ $random -eq $ispresent ]
+then
+    echo "employee is present";
+else
+    echo "employee is abscent";
+
+fi
